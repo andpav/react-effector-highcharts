c8f721b27c204180240306c122deddee13c8fe50
Andrey Pavlenko <pavlenkoandreyspb@gmail.com>
2022-08-22 11:55:34 +0300

    small fixes

--- src/features/model/api/index.ts
+++ src/features/model/api/index.ts
@@ -3,22 +3,16 @@ import axios from 'axios'
 export type ChartDataItem = [number, number]
 export type ChartDataResult = Array<ChartDataItem>
 
-export const getChartData = async ({
-                                     start,
-                                     end
-                                   }: {
-  start: number
-  end: number
-}): Promise<ChartDataResult> => {
-  const {data} = await axios.request<Array<{ date: number; weightedAverage: number }> | { error: string }>({
+export const getChartData = async ({ start, end }: { start: number; end: number }): Promise<ChartDataResult> => {
+  const { data } = await axios.request<Array<{ date: number; weightedAverage: number }> | { error: string }>({
     url: 'https://poloniex.com/public',
     params: {
       command: 'returnChartData',
       currencyPair: 'USDT_BTC',
       start,
       end,
-      period: 86400
-    }
+      period: 86400,
+    },
   })
   if (!Array.isArray(data)) return []
   return data.map((item) => [item.date * 1000, item.weightedAverage])
